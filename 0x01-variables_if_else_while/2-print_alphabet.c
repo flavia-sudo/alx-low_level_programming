4f068f6a008956f067bbb1a2c122e21fc81b70d1
flavia-sudo <kihahuflavia@gmail.com>
2023-06-17 11:20:54 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz"
-		int i;
-		for(i = 0; i < 26; i++)
-		{
-			putchar(alp[i]);
-		}
-		putchar('\n');
-		return (0);
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for(i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
 
