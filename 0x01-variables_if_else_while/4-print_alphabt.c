c268e860a64b67121c41f443c1cee291ff596972
flavia-sudo <kihahuflavia@gmail.com>
2023-06-17 14:36:20 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in lowercase without q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc < = 'z'; lc != q; lc != e; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
