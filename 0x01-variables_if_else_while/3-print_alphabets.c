2c71b7f850d0e0d40a01c099c71471d4cc25d6bc
flavia-sudo <kihahuflavia@gmail.com>
2023-06-17 12:28:07 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase and uppercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	for (i = 'a'; i < = 'Z'; i++);
+
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
