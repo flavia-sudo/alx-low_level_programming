014b8838bf01845ff29ca4e9aae3eef689b9f0cb
flavia-sudo <kihahuflavia@gmail.com>
2023-06-21 09:00:22 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Main - prints the sum of all the multiples of 3 or 5 below 1024
+ * main - prints the sum of all the multiples of 3 or 5 below 1024
  * Return: Always (Success)
  */
 
