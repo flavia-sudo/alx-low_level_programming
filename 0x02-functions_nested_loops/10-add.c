14223bbdab2a8fe2cf0206bce02eefa121d33959
flavia-sudo <kihahuflavia@gmail.com>
2023-06-21 06:27:57 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two integers
+ * @a: The first number to be computed
+ * @b: The second number to be computed
+ * Return: Integer value
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
