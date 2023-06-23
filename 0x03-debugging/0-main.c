c91f4f9771eccbc934bc08d2b5f349ab6a4b9e32
flavia-sudo <kihahuflavia@gmail.com>
2023-06-23 08:18:14 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test that the function positive_or_negative()
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
