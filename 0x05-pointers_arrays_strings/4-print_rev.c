05f3822a8831d88300d11d4f4b1a29670772dae2
flavia-sudo <kihahuflavia@gmail.com>
2023-06-26 11:26:22 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: String to print.
+ */
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s--);
+	}
+	_putchar('\n')
+}
