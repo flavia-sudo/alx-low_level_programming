0f2a4e5326a3c8a6a2ee6c0dcdaadf7c6d210405
flavia-sudo <kihahuflavia@gmail.com>
2023-07-19 12:40:37 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - prints a name.
+ * print_name - prints a name using pointer to function
  * @name: string to add
  * @f: pointer to function
  * Return: nothing
