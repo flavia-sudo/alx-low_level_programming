c2cf56d97f8dfb1335bd337c290912556625649e
flavia-sudo <kihahuflavia@gmail.com>
2023-07-21 12:34:30 -0700

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
  * Return: Always 0
  */
+
 int main(void)
 {
 	int count;
@@ -32,7 +34,9 @@ int main(void)
 		}
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
-		printf(", ");
+		{
+			printf(", ");
+		}
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
