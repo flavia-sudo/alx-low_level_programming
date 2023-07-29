4c4a485489992cd6bff772a5c06b4014b197713a
flavia-sudo <kihahuflavia@gmail.com>
2023-07-29 15:59:21 +0300

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,8 @@
  * Return: Nothing
  */
 void myStartupFun(void) __attribute__ ((constructor));
+
+void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n" "I bore my house upon my back!\n");
 }
